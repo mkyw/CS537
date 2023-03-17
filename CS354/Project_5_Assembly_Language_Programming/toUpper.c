92303e5f180568e4589077e057f6b864470bb600
mkyw <50996914+mkyw@users.noreply.github.com>
2023-03-17 02:48:41 -0500

    Delete CS354 directory

--- CS354/Project_5_Assembly_Language_Programming/toUpper.c
+++
@@ -1,8 +0,0 @@
-void To_Upper(char *str) {
-        while (*str != '\0') {
-                if (*str >= 97 && *str <= 122) {
-                        *str -= 32;
-                }
-                str++;
-        }
-}
